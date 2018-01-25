a26b70469eb2cbc421dba17c4066e643f245ecc1
IcemanNeo <nemodreammaster@gmail.com>
2018-01-24 22:24:49 -0600

    Update Mini RPS
    Rock, Paper, Broken! No it's Fixed!

--- src/MiniRPS.java
+++ src/MiniRPS.java
@@ -9,22 +9,17 @@ public class MiniRPS {
 
         while(user != 4) {
             System.out.print("Mini RPS -- [R,P,S or (E)xit]: ");
-            String input = sc.next().toLowerCase();
-            int comp = rand.nextInt(3 + 1 - 1) + 1;
 
-            if(input == "r") {
-                user = 1;
-            } else if(input == "p") {
-                user = 2;
-            } else if(input == "s") {
-                user = 3;
-            } else if(input == "e") {
-                user = 4;
-            } else {
-                System.out.println("RPS - Invalid Input");
+            switch(sc.next().toLowerCase()) {
+                case "r": user = 1; break;
+                case "p": user = 2; break;
+                case "s": user = 3; break;
+                case "e": user = 4; break;
+                default: user = 0; System.out.println("Invalid Input.."); break;
             }
 
             if (user > 0 && user < 4) {
+                int comp = rand.nextInt(3 + 1 - 1) + 1;
                 if (user == 1 && comp == 3) {
                     System.out.print("You Win! User = R -- Comp = S");
                 } else if (user == 2 && comp == 1) {
@@ -32,22 +27,16 @@ public class MiniRPS {
                 } else if (user == 3 && comp == 2) {
                     System.out.print("You Win! User = S -- Comp = P");
                 } else if (user == comp) {
-                    System.out.print("Tie!");
-                    if(user == 1) {
-                        System.out.print(" Both = R");
-                    } else if(user == 2) {
-                        System.out.print(" Both = P");
-                    } else if(user == 3) {
-                        System.out.print(" Both = S");
+                    switch(user) {
+                        case 1: System.out.print("Tie! Both = R"); break;
+                        case 2: System.out.print("Tie! Both = P"); break;
+                        case 3: System.out.print("Tie! Both = S"); break;
                     }
                 } else {
-                    System.out.print("You Lose!");
-                    if(comp == 1) {
-                        System.out.print(" User = S -- Comp = R");
-                    } else if(comp == 2) {
-                        System.out.print(" User = R -- Comp = P");
-                    } else if(comp == 3) {
-                        System.out.print(" User = P -- Comp = S");
+                    switch(comp) {
+                        case 1: System.out.print("You Lose! User = S -- Comp = R"); break;
+                        case 2: System.out.print("You Lose! User = R -- Comp = P"); break;
+                        case 3: System.out.print("You Lose! User = P -- Comp = S"); break;
                     }
                 }
                 System.out.print("\n\n");
